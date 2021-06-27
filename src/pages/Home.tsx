9f05cffcd62aa7ce48526c7d010d630eb5fa2bd0
Higor Matheus <hmoliveira83@gmail.com>
2021-06-27 13:02:16 -0300

    finalizando desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,40 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    console.log(newTaskTitle);
+    if (!newTaskTitle) return null;
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
     //TODO - add new task if it's not empty
+    setTasks(state => [...state, data]);
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+
+    setTasks(state =>
+      state.map(task => {
+        if (task.id === id) {
+          return {
+            ...task,
+            done: !task.done,
+          };
+        }
+        return task;
+      }),
+    );
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(state => state.filter(t => t.id !== id));
   }
 
   return (
@@ -31,11 +53,11 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
-  )
+  );
 }
